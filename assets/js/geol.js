5a37139d4d193324d6a0f62c789e858ce744ed75
ruservis <brewbaker@yandex.ru>
2017-03-21 15:16:39 +0300

    Update geol.js

--- assets/js/geol.js
+++ assets/js/geol.js
@@ -1,40 +1,6 @@
 ymaps.ready(function () {
            
-           
-           
-        var myMap = new ymaps.Map('map', {
-            center: [55.751574, 37.573856],
-            zoom: 9
-        }, {
-            searchControlProvider: 'yandex#search'
-        }),
-        myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
-            hintContent: 'Собственный значок метки',
-            balloonContent: 'Это красивая метка'
-        }, {
-            // Опции.
-            // Необходимо указать данный тип макета.
-            iconLayout: 'default#image',
-            // Своё изображение иконки метки.
-            iconImageHref: 'images/myIcon.gif',
-            // Размеры метки.
-            iconImageSize: [30, 42],
-            // Смещение левого верхнего угла иконки относительно
-            // её "ножки" (точки привязки).
-            iconImageOffset: [-5, -38]
-        });
-
-    myMap.geoObjects.add(myPlacemark),     
-           
-           
-           
-           
-           
-           
-           
-           
-           
-           
+ 
     var myMap,
         service = new GeolocationService(),
         myLocation = service.getLocation({
@@ -58,7 +24,24 @@ ymaps.ready(function () {
             center: myCoords,
             zoom: loc.zoom || 9,
             behaviors: ['default', 'scrollZoom']
+        }),
+                    myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+            hintContent: 'Собственный значок метки',
+            balloonContent: 'Это красивая метка'
+        }, {
+            // Опции.
+            // Необходимо указать данный тип макета.
+            iconLayout: 'default#image',
+            // Своё изображение иконки метки.
+            iconImageHref: 'images/myIcon.gif',
+            // Размеры метки.
+            iconImageSize: [30, 42],
+            // Смещение левого верхнего угла иконки относительно
+            // её "ножки" (точки привязки).
+            iconImageOffset: [-5, -38]
         });
+
+  
 
         myMap.geoObjects.add(myPlacemark);
     });
