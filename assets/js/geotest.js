86da5774f2da4e4dd678ab159c221e6ff784180d
ruservis <brewbaker@yandex.ru>
2017-03-21 21:55:14 +0300

    Update geotest.js

--- assets/js/geotest.js
+++ assets/js/geotest.js
@@ -1,4 +1,36 @@
 ymaps.ready(function () {
+    
+    
+    
+    
+     // Создание метки с круглой активной областью.
+    var circleLayout = ymaps.templateLayoutFactory.createClass('<div class="placemark_layout_container"><div class="circle_layout">#</div></div>');
+
+    var circlePlacemark = new ymaps.Placemark(
+        [55.783202, 37.605584], {
+            hintContent: 'Метка с круглым HTML макетом'
+        }, {
+            iconLayout: circleLayout,
+            // Описываем фигуру активной области "Круг".
+            iconShape: {
+                type: 'Circle',
+                // Круг описывается в виде центра и радиуса
+                coordinates: [0, 0],
+                radius: 25
+            }
+        }
+    );
+    map.geoObjects.add(circlePlacemark),
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
     var map;
     ymaps.geolocation.get().then(function (res) {
         var mapContainer = $('#map'),
@@ -22,21 +54,3 @@ ymaps.ready(function () {
     }
 });
 
- // Создание метки с круглой активной областью.
-    var circleLayout = ymaps.templateLayoutFactory.createClass('<div class="placemark_layout_container"><div class="circle_layout">#</div></div>');
-
-    var circlePlacemark = new ymaps.Placemark(
-        [55.783202, 37.605584], {
-            hintContent: 'Метка с круглым HTML макетом'
-        }, {
-            iconLayout: circleLayout,
-            // Описываем фигуру активной области "Круг".
-            iconShape: {
-                type: 'Circle',
-                // Круг описывается в виде центра и радиуса
-                coordinates: [0, 0],
-                radius: 25
-            }
-        }
-    );
-    map.geoObjects.add(circlePlacemark);
