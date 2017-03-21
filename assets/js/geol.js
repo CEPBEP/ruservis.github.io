4ae20fa9864da7a1308a11f0f4c31c8d16d23825
ruservis <brewbaker@yandex.ru>
2017-03-21 15:12:34 +0300

    Update geol.js

--- assets/js/geol.js
+++ assets/js/geol.js
@@ -1,63 +1,41 @@
 ymaps.ready(function () {
-           // Создаем геообъект с типом геометрии "Точка".
-        myGeoObject = new ymaps.GeoObject({
-            // Описание геометрии.
-            geometry: {
-                type: "Point",
-                coordinates: [55.8, 37.8]
-            },
-            // Свойства.
-            properties: {
-                // Контент метки.
-                iconContent: 'Метка',
-                balloonContent: 'Меня можно перемещать'
-            }
+           
+           
+           
+        var myMap = new ymaps.Map('map', {
+            center: [55.751574, 37.573856],
+            zoom: 9
         }, {
-            // Опции.
-            // Иконка метки будет растягиваться под размер ее содержимого.
-            preset: 'twirl#redStretchyIcon',
-            // Метку можно перемещать.
-            draggable: true
+            searchControlProvider: 'yandex#search'
         }),
-
-        // Создаем метку с помощью вспомогательного класса.
-        myPlacemark1 = new ymaps.Placemark([55.8, 37.6], {
-            // Свойства.
-            // Содержимое иконки, балуна и хинта.
-            iconContent: '1',
-            balloonContent: 'Балун',
-            hintContent: 'Стандартный значок метки'
+        myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+            hintContent: 'Собственный значок метки',
+            balloonContent: 'Это красивая метка'
         }, {
             // Опции.
-            // Стандартная фиолетовая иконка.
-            preset: 'twirl#violetIcon'
-        }),
-
-        myPlacemark2 = new ymaps.Placemark([55.76, 37.56], {
-            // Свойства.
-            hintContent: 'Собственный значок метки'
-        }, {
-            // Опции.
+            // Необходимо указать данный тип макета.
+            iconLayout: 'default#image',
             // Своё изображение иконки метки.
             iconImageHref: 'images/myIcon.gif',
             // Размеры метки.
             iconImageSize: [30, 42],
             // Смещение левого верхнего угла иконки относительно
             // её "ножки" (точки привязки).
-            iconImageOffset: [-3, -42]
+            iconImageOffset: [-5, -38]
         });
 
-    // Добавляем все метки на карту.
-    myMap.geoObjects
-        .add(myPlacemark1)
-        .add(myPlacemark2)
-        .add(myGeoObject);
-};
-
-    
-    
-    
-    
+    myMap.geoObjects.add(myPlacemark);       
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+  
     var myMap,
         service = new GeolocationService(),
         myLocation = service.getLocation({
